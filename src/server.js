3e84031f42e6a2e41995fe244d653739d60f6458
Mateus Ribeiro <98188179+mtsribeiro@users.noreply.github.com>
2022-09-02 15:30:02 -0300

    .
    .

---
+++ src/server.js
@@ -0,0 +1,40 @@
+const { BrowserWindow, Notification } = require("electron");
+const {getConnection} = require('./database');
+const http = require('http');
+const express = require('express');
+const app = express();
+const path = require('path');
+const server = http.createServer(app);
+
+const port = 9000;
+
+app.use('/', express.static(path.join(__dirname, '../ui')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../ui/home.html'))
+})
+
+function createWindow() {
+    window = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        frame: false,
+        show: false,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+          }
+    })
+    window.loadURL(`http://localhost:${port}`);
+    window.once('ready-to-show', () => {
+        window.show()
+      })
+}
+
+server.listen(port, () => {
+    console.log(`Servidor web em execução: http://localhost:${port}`);
+});
+
+module.exports = {
+    createWindow
+}
